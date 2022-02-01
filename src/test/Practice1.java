362b6ee361c5707d65f86617edaaaee83a5d4abd
mahes <mahes@DESKTOP-3NFVDKQ>
2022-02-01 12:01:47 +0530

    hiiiiiii

--- src/test/Practice1.java
+++ src/test/Practice1.java
@@ -4,7 +4,7 @@ public class Practice1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("user 1 pass");
+		System.out.println("user 1pass");
 	}
 
 }
